2e80a04fbbaaa1cddff6e8fcbabbf7b3dc8fd6dc
Da_Dog <jiawenli233@outlook.com>
2024-04-13 22:50:59 -0700

    cleaned up frontend changed tournament ID to UUID

---
+++ frontend/src/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Welcome!</h1>
+            <h2>Enter your participant ID or select a tournament to continue :)</h2>
+        </>
+    )
+}
+
+export default Home;
--- tournament/models.py
+++ tournament/models.py
@@ -4,7 +4,7 @@ import uuid
 
 # Create your models here.
 class Tournament(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     start_date = models.DateTimeField()
